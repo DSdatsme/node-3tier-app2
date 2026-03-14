9fb67e318f959dfbe1709ec982120449f2fea3dd
Darshit Suratwala <dsdatsme@gmail.com>
2026-03-14 17:50:08 +0530

    feat: adding GH workflows for image build and push

---
+++ web/eslint.config.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules/**"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "no-console": "off",
+      "eqeqeq": "error",
+      "no-var": "warn",
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        console: "readonly",
+        describe: "readonly",
+        it: "readonly",
+      },
+    },
+  },
+];
